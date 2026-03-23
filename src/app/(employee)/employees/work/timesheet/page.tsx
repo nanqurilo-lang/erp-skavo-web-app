221f8b6facdbfbcf7e5239e5680ad58f20a6ca9c
nanqurilo-lang <nanqurilo@gmail.com>
2026-03-23 13:10:25 +0530

    fix view modal of timesheet in employee panel

--- src/app/(employee)/employees/work/timesheet/page.tsx
+++ src/app/(employee)/employees/work/timesheet/page.tsx
@@ -1450,13 +1450,16 @@ export default function TimesheetPage() {
 
       {/* View Modal */}
       {isViewOpen && selectedRow && (
-        <div className="fixed inset-0 z-[10030] flex items-start justify-center pt-8 px-6">
+        // <div className="fixed inset-0 z-[10030] flex items-start justify-center pt-8 px-6">
+        <div className="fixed inset-0 z-[10020] flex justify-end">
           <div
             className="absolute inset-0 bg-black/40"
             onClick={() => setIsViewOpen(false)}
           />
 
-          <div className="relative bg-white w-full max-w-4xl rounded-xl shadow-xl overflow-hidden">
+          {/* <div className="relative bg-white w-full max-w-4xl rounded-xl shadow-xl overflow-hidden"> */}
+                    <div className="relative w-[83%] h-full bg-white shadow-xl flex flex-col overflow-y-auto">
+
             <div className="flex items-center justify-between px-6 py-4 border-b">
               <h3 className="text-xl font-semibold">Timesheet </h3>
               <button
